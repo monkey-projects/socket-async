b82f94a57c599e36f9c68195fd62c43a9311dcce
Wout Neirynck <wout@monkey-projects.be>
2023-09-15 09:31:48 +0200

    Improved error handling

--- test/monkey/socket_async/test/core_test.clj
+++ test/monkey/socket_async/test/core_test.clj
@@ -5,16 +5,32 @@
              [core :as sut]
              [uds :as uds]]))
 
+(defn with-sockets
+  "Opens listening socket and connects a client socket to it.
+   Invokes `f` with a map that holds `:listener`, `:clietn` and
+   `:server`."
+  [f]
+  (let [path (format "test=%s.sock" (random-uuid))
+        addr (uds/make-address path)
+        listener (uds/listen-socket addr)
+        client (uds/connect-socket addr)
+        server (uds/accept listener)]
+    (is (uds/connected? client))
+    (is (uds/connected? server))
+    (try
+      (f {:listener listener
+          :client client
+          :server server})
+      (finally
+        ;; Clean up
+        (uds/close client)
+        (uds/close server)
+        (uds/delete-address path)))))
+
 (deftest read-and-write
   (testing "can send objects between channels using socket"
-    (let [path "test.sock"
-          addr (uds/make-address path)
-          listener (uds/listen-socket addr)
-          client (uds/connect-socket addr)
-          server (uds/accept listener)]
-      (is (uds/connected? client))
-      (is (uds/connected? server))
-      (try
+    (with-sockets
+      (fn [{:keys [client server]}]
         (let [obj {:message "Hi, this is a test"}
               in (ca/chan)
               out (ca/chan)]
@@ -26,9 +42,4 @@
             (is (= in p) "did not expect timeout")
             (is (= obj v)) "did not receive the object sent")
           (ca/close! in)
-          (ca/close! out))
-        (finally
-          ;; Clean up
-          (uds/close client)
-          (uds/close server)
-          (uds/delete-address path))))))
+          (ca/close! out))))))
